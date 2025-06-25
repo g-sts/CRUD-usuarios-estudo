5b2a6b164acfbcef3ef5024317802b1ec537ab33
git status <215340863+g-sts@users.noreply.github.com>
2025-06-25 20:54:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,54 @@ app.post('/usuarios', async (req, res) => {
     
 })
 
-app.get('/usuarios', (req, res) =>{
+app.get('/usuarios', async (req, res) => {
+
+    let users = []
+    if (req.query){
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                age: req.query.age,
+            }
+        })
+
+    } else{
+        users = await prisma.user.findMany()
+
+    }
 
     res.status(200).json(users)
+})
+
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where:{
+            id:req.params.id 
+        },
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+    
+})
+
+app.delete('/usuarios/:id', async(req, res) =>{
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'Usuário deletado com sucesso!'})
 
 })
+
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
