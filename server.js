91fe05755d992dcb2a7eaff6edcdfcd04dafbb45
git status <215340863+g-sts@users.noreply.github.com>
2025-06-25 20:21:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.post('/usuarios', (req, res) =>{
 
     users.push(req.body)
 
-    res.send('Ok post')
+    res.status(201).json(req.body)
 
 })
 
 app.get('/usuarios', (req, res) =>{
 
-    res.json(users)
+    res.status(200).json(users)
 
 })
 
